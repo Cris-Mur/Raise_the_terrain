5ae857a7465d7143fadf995bd7555d9d93718411
Cris-Mur <yomy98@gmail.com>
2020-08-04 20:01:21 -0500

    change permissions

--- Source/_init_map.c
+++ Source/_init_map.c
@@ -9,38 +9,37 @@
  */
 int init_map(SDL_world_t **pWorld)
 {
-    FILE *log = NULL;
-    size_t lenny = 0, w = 0, h = 0;
-    char *buffersito = NULL, *tk = NULL;
-    SDL_world_t *tmp = NULL;
+	FILE *log = NULL;
+	size_t lenny = 0, w = 0, h = 0;
+	char *buffersito = NULL, *tk = NULL;
+	SDL_world_t *tmp = NULL;
 
-    tmp = *pWorld;
-    tmp = file_catch(&tmp);
-
-    if (tmp->path) {
-        log = fopen(tmp->path, "r");
-        if (!log) {
-            write(STDERR_FILENO,
-                  "ERROR can't open file...\n", 25);
-            return (-1);
-        }
-        //get size of the matrix
-        for (h = 0; getline(&buffersito, &lenny, log) != -1; h++)
-        {
-            tk = strtok(buffersito, " \n");
-            for (w = 0; tk; w++)
-            {
-                tmp->map[h][w].x = (_SCREEN_WIDTH / (tmp->px + 8)) * (h + 1);
-                tmp->map[h][w].y = (_SCREEN_HEIGHT / (tmp->py + 3)) * (w + 1);
-                tmp->map[h][w].z = (atof(tk)) * 2;
-                tk = strtok(NULL, " \n");
-            }
-        }
-        free(buffersito);
-        fclose(log);
-    }
-    *pWorld = tmp;
-    return (1);
+	tmp = *pWorld;
+	tmp = file_catch(&tmp);
+	if (tmp->path) {
+		log = fopen(tmp->path, "r");
+		if (!log) {
+			write(STDERR_FILENO,
+			      "ERROR can't open file...\n", 25);
+			return (-1);
+		}
+		//get size of the matrix
+		for (h = 0; getline(&buffersito, &lenny, log) != -1; h++)
+		{
+			tk = strtok(buffersito, " \n");
+			for (w = 0; tk; w++)
+			{
+				tmp->map[h][w].x = (_SCREEN_WIDTH / (tmp->px + 8)) * (h + 1);
+				tmp->map[h][w].y = (_SCREEN_HEIGHT / (tmp->py + 3)) * (w + 1);
+				tmp->map[h][w].z = (atof(tk)) * 2;
+				tk = strtok(NULL, " \n");
+			}
+		}
+		free(buffersito);
+		fclose(log);
+	}
+	*pWorld = tmp;
+	return (1);
 }
 
 /**
@@ -50,58 +49,58 @@ int init_map(SDL_world_t **pWorld)
  */
 SDL_world_t *file_catch(SDL_world_t **pWorld)
 {
-    FILE *log;
-    size_t lenny, lennx, tblk = 0;
-    char *buffersito, *tk;
-    SDL_world_t *tmp = NULL;
+	FILE *log;
+	size_t lenny, lennx, tblk = 0;
+	char *buffersito, *tk;
+	SDL_world_t *tmp = NULL;
 
-    tmp = *pWorld;
-    if (tmp->path)
-    {
-        log = fopen(tmp->path, "r");
-        if (!log)
-        {
-            write(STDERR_FILENO,
-                    "ERROR can't open file...\n", 25);
-            return (NULL);
-        }
-        //get size of the matrix
-        for (; getline(&buffersito, &lenny, log) != -1;
-        tmp->py++)
-            if (tmp->py == 0)
-            {
-                tk = strtok(buffersito, " \n");
-                for (; tk; tmp->px++)
-                    tk = strtok(NULL, " \n");
-            }
-        //allocated matrix [part_y]
-        tmp->map = malloc(sizeof(cartesian_t *) * tmp->py);
-        if (!tmp->map)
-        {
-            write(STDERR_FILENO,
-                    "Allocate map[y] FAILED...\n", 23);
-            return (NULL);
-        }
-        //allocated matrix [y][part_x]
-        for (lenny = 0; lenny < tmp->py; lenny++)
-        {
-            tmp->map[lenny] = malloc(sizeof(cartesian_t) * tmp->px);
-            if (!tmp->map[lenny])
-            {
-                write(STDERR_FILENO,
-                      "Allocate map[y][x] FAILED...\n", 23);
-                return (NULL);
-            }
-            for (lennx = 0; lennx < tmp->px; lennx++)
-            {
-                tmp->map[lenny][lennx].x = 0;
-                tmp->map[lenny][lennx].y = 0;
-                tmp->map[lenny][lennx].z = 0;
-                tblk++;
-            }
-        }
-        free(buffersito);
-        fclose(log);
-    }
-    return (tmp);
+	tmp = *pWorld;
+	if (tmp->path)
+	{
+		log = fopen(tmp->path, "r");
+		if (!log)
+		{
+			write(STDERR_FILENO,
+			      "ERROR can't open file...\n", 25);
+			return (NULL);
+		}
+		//get size of the matrix
+		for (; getline(&buffersito, &lenny, log) != -1;
+		     tmp->py++)
+			if (tmp->py == 0)
+			{
+				tk = strtok(buffersito, " \n");
+				for (; tk; tmp->px++)
+					tk = strtok(NULL, " \n");
+			}
+		//allocated matrix [part_y]
+		tmp->map = malloc(sizeof(cartesian_t *) * tmp->py);
+		if (!tmp->map)
+		{
+			write(STDERR_FILENO,
+			      "Allocate map[y] FAILED...\n", 23);
+			return (NULL);
+		}
+		//allocated matrix [y][part_x]
+		for (lenny = 0; lenny < tmp->py; lenny++)
+		{
+			tmp->map[lenny] = malloc(sizeof(cartesian_t) * tmp->px);
+			if (!tmp->map[lenny])
+			{
+				write(STDERR_FILENO,
+				      "Allocate map[y][x] FAILED...\n", 23);
+				return (NULL);
+			}
+			for (lennx = 0; lennx < tmp->px; lennx++)
+			{
+				tmp->map[lenny][lennx].x = 0;
+				tmp->map[lenny][lennx].y = 0;
+				tmp->map[lenny][lennx].z = 0;
+				tblk++;
+			}
+		}
+		free(buffersito);
+		fclose(log);
+	}
+	return (tmp);
 }
